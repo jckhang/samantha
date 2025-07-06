76cc64899955d771cb6aad1ab8dcd53f411805a5
Yuxiang Zhang <yuxiang.zhang42@gmail.com>
2025-07-06 22:34:31 +0800

    chore: 更新 ic_launcher 图标资源文件，保持与设计稿一致

--- samantha-simple/android-app/app/build/generated/hilt/component_trees/release/dagger/hilt/internal/processedrootsentinel/codegen/_com_samantha_SamanthaApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.samantha.SamanthaApplication"
-)
-public final class _com_samantha_SamanthaApplication {
-}
